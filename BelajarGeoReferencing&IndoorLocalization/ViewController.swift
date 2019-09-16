fd74a37f69802fe3f2d44c9cdcf533eb1a48167b
Muhammad Rajab <ichijo_rajab@hotmail.com>
2019-09-16 18:37:56 +0700

    Initial Commit

---
+++ BelajarGeoReferencing&IndoorLocalization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelajarGeoReferencing&IndoorLocalization
+//
+//  Created by Muhammad Rajab Priharsanto on 16/09/19.
+//  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
