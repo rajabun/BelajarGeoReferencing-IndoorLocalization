fafd998e3d1453467ce45f04cae5093a7a079963
Muhammad Rajab <ichijo_rajab@hotmail.com>
2019-09-18 12:40:23 +0700

    Georeferencing contoh 1 berhasil

--- BelajarGeoReferencing&IndoorLocalization/ViewController.swift
+++ BelajarGeoReferencing&IndoorLocalization/ViewController.swift
@@ -4,77 +4,40 @@
 //
 //  Created by Muhammad Rajab Priharsanto on 16/09/19.
 //  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
-//
+//  Source : https://medium.com/@fede_nieto/geofences-how-to-implement-virtual-boundaries-in-the-real-world-f3fc4a659d40
 
 import UIKit
 import MapKit
 import CoreLocation
+import UserNotifications
 
-class ViewController: UIViewController, CLLocationManagerDelegate
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate
 {
-
     @IBOutlet weak var tampilanPeta: MKMapView!
     
     //bikin objek untuk location manager
-    let locationManager = CLLocationManager()
+    var locationManager: CLLocationManager = CLLocationManager()
+    
+    @IBOutlet weak var statusLokasiLabel: UILabel!
+    
+    let ENTERED_REGION_MESSAGE = "Selamat Datang di Akademi"
+    let ENTERED_REGION_NOTIFICATION_ID = "EnteredRegionNotification"
+    let EXITED_REGION_MESSAGE = "Sampai Jumpa Kembali"
+    let EXITED_REGION_NOTIFICATION_ID = "ExitedRegionNotification"
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        checkLocationServices()
-        // Do any additional setup after loading the view.
-    }
-
-    func checkLocationServices()
-    {
-        if CLLocationManager.locationServicesEnabled()
-        {
-            setupLocationManager()
-            //locationManager.requestAlwaysAuthorization()
-            checkLocationAuthorization()
-            let geoFenceRegion:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(-6.3023, 106.6522), radius: 10, identifier: "Boise")
-            
-            locationManager.startMonitoring(for: geoFenceRegion)
-        }
-        else
-        {
-            //show alert ngasitau kalo dia ga ngaktifin location
-        }
-    }
-    
-    func setupLocationManager()
-    {
-        locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    }
-    
-    func checkLocationAuthorization()
-    {
-        switch CLLocationManager.authorizationStatus()
-        {
-        case.authorizedWhenInUse:
-            // do map stuff
-            print("Masuk Pak Eko")
-            tampilanPeta.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
-            break
-        case.denied:
-            // show alert and suggest them to turn on location services
-            break
-        case.notDetermined:
-            // ask for permission to use the location
-            locationManager.requestWhenInUseAuthorization()
-            break
-        case.restricted:
-            // tell them what's up, restricted ini kalo ditolak dari parental control untuk ganti status.
-            break
-        case.authorizedAlways:
-            // sebaiknya ga trlalu digunakan karena sekarang user sangat menjaga privasinya.
-            break
-        default:
-            break
-        }
+        tampilanPeta.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        
+        self.tampilanPeta.delegate = self
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.badge, .alert, .sound]) { (granted, error) in }
     }
     
     func centerViewOnUserLocation()
@@ -84,45 +47,95 @@ class ViewController: UIViewController, CLLocationManagerDelegate
             let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 10000, longitudinalMeters: 10000)
             tampilanPeta.setRegion(region, animated: true)
         }
-        
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    func setUpGeofenceForPlayaGrandeBeach()
     {
-        guard let location = locations.last
-            else
-        {
-            return
-        }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: 10000, longitudinalMeters: 10000)
-        tampilanPeta.setRegion(region, animated: true)
+        //radius 100 itu sampe ke the breeze dan unilever
+        let geofenceRegionCenter = CLLocationCoordinate2DMake(-6.3023, 106.6522);
+        let geofenceRegion = CLCircularRegion(center: geofenceRegionCenter, radius: 100, identifier: "CurrentLocation");
+        geofenceRegion.notifyOnExit = true;
+        geofenceRegion.notifyOnEntry = true;
+        self.locationManager.startMonitoring(for: geofenceRegion)
         
-//        let annotation = MKPointAnnotation()
-//        annotation.coordinate = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-//        annotation.title = "Green Office Park"
-//        annotation.subtitle = "BSD, Indonesia"
-//        tampilanPeta.addAnnotation(annotation)
+        //Buat tampilan di petanya
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let mapRegion = MKCoordinateRegion(center: geofenceRegionCenter, span: span)
+        self.tampilanPeta.setRegion(mapRegion, animated: true)
         
-        for currentLocation in locations
-        {
-            //print("\(index): \(currentLocation)")
-        }
+        let regionCircle = MKCircle(center: geofenceRegionCenter, radius: 400)
+        self.tampilanPeta.addOverlay(regionCircle)
+        self.tampilanPeta.showsUserLocation = true;
     }
     
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion)
     {
-        checkLocationAuthorization()
+        print("Started Monitoring Region: \(region.identifier)")
+        statusLokasiLabel.text = "Monitoring Region"
     }
     
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion)
     {
-        print("Entered: \(region.identifier)")
+        print(ENTERED_REGION_MESSAGE)
+        statusLokasiLabel.text = ENTERED_REGION_MESSAGE
+        //Good place to schedule a local notification
+        
+        self.createLocalNotification(message: ENTERED_REGION_MESSAGE, identifier: ENTERED_REGION_NOTIFICATION_ID)
     }
     
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion)
     {
-        print("Exited: \(region.identifier)")
+        print(EXITED_REGION_MESSAGE)
+        statusLokasiLabel.text = EXITED_REGION_MESSAGE
+        //Good place to schedule a local notification
+        
+        self.createLocalNotification(message: EXITED_REGION_MESSAGE, identifier: EXITED_REGION_NOTIFICATION_ID)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        //self.statusLokasiLabel.text = "Update Location"
+    }
+    
+    //Kalo mau pake geofencing, harus banget pilih always in use (opsi kedua)
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
+    {
+        if (status == CLAuthorizationStatus.authorizedAlways)
+        {
+            //App Authorized, stablish geofence
+            self.setUpGeofenceForPlayaGrandeBeach()
+        }
+        else if (status == CLAuthorizationStatus.authorizedWhenInUse)
+        {
+            //App Authorized, stablish geofence
+            self.setUpGeofenceForPlayaGrandeBeach()
+        }
+    }
+    
+    func createLocalNotification(message: String, identifier: String)
+    {
+        //Create a local notification
+        let content = UNMutableNotificationContent()
+        content.body = message
+        content.sound = UNNotificationSound.default
+        
+        // Deliver the notification
+        let request = UNNotificationRequest.init(identifier: identifier, content: content, trigger: nil)
+        
+        // Schedule the notification
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in }
+    }
+    
+    //MARK - MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer
+    {
+        let overlayRenderer : MKCircleRenderer = MKCircleRenderer(overlay: overlay);
+        overlayRenderer.lineWidth = 4.0
+        overlayRenderer.strokeColor = UIColor(red: 7.0/255.0, green: 106.0/255.0, blue: 255.0/255.0, alpha: 1)
+        overlayRenderer.fillColor = UIColor(red: 0.0/255.0, green: 203.0/255.0, blue: 208.0/255.0, alpha: 0.7)
+        return overlayRenderer
     }
 }
 
